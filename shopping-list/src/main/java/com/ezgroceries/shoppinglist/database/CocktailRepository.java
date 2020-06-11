f7603a6fe2e73a19e187dbeefbceb2867d350071
jeroenvdw <jeroen.vandewal@gmail.com>
2020-06-11 16:56:02 +0200

    typo

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/database/CocktailRepository.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/database/CocktailRepository.java
@@ -1,10 +1,10 @@
 package com.ezgroceries.shoppinglist.database;
 
-import com.ezgroceries.shoppinglist.database.entities.ShoppingListEntity;
+import com.ezgroceries.shoppinglist.database.entities.CocktailEntity;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CocktailRepository extends JpaRepository<ShoppingListEntity, UUID> {
+public interface CocktailRepository extends JpaRepository<CocktailEntity, UUID> {
 }
